e27e10890868b0ea162d01c9cfd0a40e50ac3be1
simonhukin <simonhukin@gmail.com>
2011-07-03 18:11:58 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CubeHolding::Application.config.secret_token = '64cd778511cbc310c9e3df1f016b1d0e7007d2f6e1baa58962c88b0496e514e8a91451eb319958b4bf9845dc195ee3826b1586110d8f7716a56638674eca8afe'
